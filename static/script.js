03bb240c3cdeb93810bc3076c272f40b8a3a012c
LOeilCritique69 <yanisfa69@gmail.com>
2025-06-17 22:49:00 +0200

    oui oui

--- static/script.js
+++ static/script.js
@@ -7,9 +7,11 @@ fetch('/api/films')
       div.className = 'film';
       div.innerHTML = `
         <img src="${film.image}" alt="${film.title}">
-        <h2>${film.title}</h2>
-        <div class="rating">★ ${film.rating || 'N/A'}</div>
-        <div class="review">${film.review}</div>
+        <div class="film-content">
+          <h2>${film.title}</h2>
+          <div class="rating">★ ${film.rating || 'N/A'}</div>
+          <div class="review">${film.review}</div>
+        </div>
       `;
       container.appendChild(div);
     });
